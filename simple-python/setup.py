b481016bb6df6effebdfd99d4e0950b80708059b
Toni Cebrián <t.cebrian@stuart.com>
2017-10-16 10:30:14 +0200

    Initial import of projects

---
+++ simple-python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hello',
+      version='0.1',
+      description='Hello greeting',
+      url='https://github.com/tonicebrian/manage-releases-example',
+      author='Toni Cebrián',
+      author_email='toni.cebrian@example.com',
+      license='MIT',
+      packages=['hello'],
+      zip_safe=False)
